6502d27ab67eb75bf2e1115ac9212970af2c8db5
Luiz Irber <irberlui@msu.edu>
2014-03-21 11:29:39 -0400

    Update sync and merge scripts

--- scripts/blat_merge_outputs.py
+++ scripts/blat_merge_outputs.py
@@ -12,9 +12,17 @@ COLUMN_NAMES = [s.strip()
 output = pd.DataFrame()
 
 for output_file in glob("../workdir/results/transc_*.filtered"):
+    print 'reading file', output_file
     table = pd.read_table(output_file, names=COLUMN_NAMES)
-    table['source'] = "_".join(os.path.splitext(os.path.basename(output_file))[0].split('_')[1:])
-    output = output.append(table)
+    source = "_".join(os.path.splitext(os.path.basename(output_file))[0].split('_')[1:])
+    table['source'] = source
+#    print 'appending source', source
+#    output = pd.concat([output, table])
+#    print 'finished appending source', source
+    table[table['alignment length'] > 200].to_hdf("minlen200.h5", source)
+    #table.to_hdf("merged.h5", source)#, mode='w', complib='blosc', complevel=9)
+
 
 #output.to_csv('merged.csv')
-output.to_hdf('merged.h5', keys=output.keys(), mode='w', complib='blosc', complevel=9)
+#print 'writing HDF5'
+#output.to_hdf('merged.h5', 'merged.h5', mode='w', complib='blosc', complevel=9)
--- sync_galGal.sh
+++ sync_galGal.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -exu
 
-rsync -azPv ~/biodata/galGal/ /mnt/scratch/irberlui/biodata/galGal/
-rsync -azPv /mnt/scratch/irberlui/biodata/galGal/ ~/biodata/galGal/
+rsync -azPv ~/biodata/galGal/ /mnt/scratch/irberlui/biodata/galGal/ --exclude={*/.git}
+rsync -azPv /mnt/scratch/irberlui/biodata/galGal/ ~/biodata/galGal/ --exclude={*/.git}
