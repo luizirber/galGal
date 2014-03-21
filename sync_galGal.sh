08ab56ca36828ee8aff163595cf9697b46edd6b7
Luiz Irber <irberlui@msu.edu>
2014-03-21 11:32:08 -0400

    Correct rsync incantation

--- sync_galGal.sh
+++ sync_galGal.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -exu
 
-rsync -azPv ~/biodata/galGal/ /mnt/scratch/irberlui/biodata/galGal/ --exclude={*/.git}
-rsync -azPv /mnt/scratch/irberlui/biodata/galGal/ ~/biodata/galGal/ --exclude={*/.git}
+rsync -azPv ~/biodata/galGal/ /mnt/scratch/irberlui/biodata/galGal/ -f"- .git/"
+rsync -azPv /mnt/scratch/irberlui/biodata/galGal/ ~/biodata/galGal/ -f"- .git/"
