ded744191e55aa4d72ad289a356869fd1693072a
Luiz Irber <irberlui@msu.edu>
2014-04-29 08:50:00 -0400

    Ortho RNA matches in genome

--- sync_galGal.sh
+++ sync_galGal.sh
@@ -3,11 +3,10 @@
 # Reference:
 # http://www.gnu.org/software/parallel/man.html#example__parallelizing_rsync
 
-export SRCDIR="/mnt/home/irberlui/biodata/galGal"
-export DESTDIR="/mnt/scratch/tg/irberlui/biodata/galGal"
+export SRCDIR="/mnt/scratch/tg/irberlui/biodata/galGal"
+export DESTDIR="/mnt/home/irberlui/biodata/galGal"
 
 cd $SRCDIR; find . -type f -path ./misc -prune -o -size +100000 | parallel -j 10 -v mkdir -p $DESTDIR/{//}\;rsync -avP {} $DESTDIR/{}
 
-rsync -av $SRCDIR/ $DESTDIR/ -f"- .git/"
-
-rsync -av $DESTDIR/ $SRCDIR/ -f"- .git/"
+#rsync -av $SRCDIR/ $DESTDIR/ -f"- .git/"
+rsync -av $SRCDIR/ $DESTDIR/
